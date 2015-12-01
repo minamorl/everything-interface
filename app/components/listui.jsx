3ea5a62ac5d6844e206ea97d3faf59f7f15fccc9
minamorl <minamorl@users.noreply.github.com>
2015-12-02 00:27:52 +0900

    automatic url tagging

--- app/components/listui.jsx
+++ app/components/listui.jsx
@@ -4,6 +4,9 @@ let React    = require('react')
 let $        = require('jquery')
 let _        = require('lodash')
 
+let autolinker = require("autolinker")
+let sanitizer = require('sanitizer')
+
 class ListUI extends React.Component {
   render () {
     let lists = []
@@ -18,8 +21,14 @@ class ListUI extends React.Component {
 
 class ListElement extends React.Component {
   render () { 
+    let linked = () => {
+
+      return {
+        __html:autolinker.link(sanitizer.escape(this.props.data.body))
+      }
+    }
     return <li>
-      <div>{this.props.data.body}</div>
+      <div dangerouslySetInnerHTML={linked()} />
       <div className="detail">
         <div className="commit-author">
           <a href={"#/thread/" + window.encodeURI(this.props.data.thread.name)}>{this.props.data.thread.name}</a>
