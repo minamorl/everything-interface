7a95db224d6cf675dfa1248ba2deda3fefa6293f
minamorl <minamorl@users.noreply.github.com>
2015-12-07 10:21:27 +0900

    style: beautify gulpfile and webpack.config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,115 +1,123 @@
 'use strict'
 
-let gulp          = require('gulp');
-let del           = require('del');
-let bower         = require('gulp-bower');
-let uglify        = require('gulp-uglify');
-let cond          = require('gulp-if');
-let gutil         = require('gulp-util');
-let runSequence   = require('run-sequence');
-let merge         = require('merge');
-let frontMatter   = require('gulp-front-matter');
-let yamlFront     = require('yaml-front-matter');
-let layout        = require('gulp-layout');
-let markdown      = require('gulp-markdown');
-let jade          = require('gulp-jade');
-let fs            = require('fs');
-let rename        = require('gulp-rename');
-let tap           = require('gulp-tap');
-let path          = require('path');
-let marked        = require('marked');
-let git           = require('gulp-git');
-let htmlmin       = require('gulp-htmlmin');
-let cssmin        = require('gulp-minify-css');
-let webserver     = require('gulp-webserver');
-let webpack       = require('webpack');
+let gulp = require('gulp');
+let del = require('del');
+let bower = require('gulp-bower');
+let uglify = require('gulp-uglify');
+let cond = require('gulp-if');
+let gutil = require('gulp-util');
+let runSequence = require('run-sequence');
+let merge = require('merge');
+let frontMatter = require('gulp-front-matter');
+let yamlFront = require('yaml-front-matter');
+let layout = require('gulp-layout');
+let markdown = require('gulp-markdown');
+let jade = require('gulp-jade');
+let fs = require('fs');
+let rename = require('gulp-rename');
+let tap = require('gulp-tap');
+let path = require('path');
+let marked = require('marked');
+let git = require('gulp-git');
+let htmlmin = require('gulp-htmlmin');
+let cssmin = require('gulp-minify-css');
+let webserver = require('gulp-webserver');
+let webpack = require('webpack');
 let webpackConfig = require("./webpack.config");
-let webpackProd   = require("./webpack.config.production");
-let cheerio       = require('cheerio');
-let striptags     = require('striptags');
+let webpackProd = require("./webpack.config.production");
+let cheerio = require('cheerio');
+let striptags = require('striptags');
 
 gulp.task('clean', () => {
-  return del(['.tmp', 'dist']);
+    return del(['.tmp', 'dist']);
 })
 
 gulp.task('build:misc', () => {
-  return gulp.src(['favicon/**/*', '*.html', 'css/**/*', 'js/**/*'], {base: "."})
-    .pipe(gulp.dest('dist'))
+    return gulp.src(['favicon/**/*', '*.html', 'css/**/*', 'js/**/*'], {
+            base: "."
+        })
+        .pipe(gulp.dest('dist'))
 });
 
 gulp.task('compress', ['compress:html', 'compress:css']);
 
 gulp.task('compress:html', () => {
-  return gulp.src('dist/**/*.html', {base: "."})
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('.'));
+    return gulp.src('dist/**/*.html', {
+            base: "."
+        })
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('.'));
 });
 
 gulp.task('compress:css', () => {
-  return gulp.src('dist/**/*.css', {base: "."})
-    .pipe(cssmin())
-    .pipe(gulp.dest('.'));
+    return gulp.src('dist/**/*.css', {
+            base: "."
+        })
+        .pipe(cssmin())
+        .pipe(gulp.dest('.'));
 });
 
 gulp.task('bower', () => {
-  return bower();
+    return bower();
 });
 
 gulp.task('default', () => {
-  return runSequence('clean', 'bower', 'build', 'webpack', 'serve');
+    return runSequence('clean', 'bower', 'build', 'webpack', 'serve');
 });
 
 gulp.task('deploy', () => {
-  return runSequence('clean', 'bower', 'build', 'compress', 'webpack:prod');
+    return runSequence('clean', 'bower', 'build', 'compress', 'webpack:prod');
 });
 
 let _webpack = (config, callback) => {
-  return webpack(config, (err, stats) => {
-    if(err)
-      throw new gutil.PluginError("webpack:build", err)
-    gutil.log("[webpack:build]", stats.toString({
-      colors: true
-    }));
-    callback()
+    return webpack(config, (err, stats) => {
+        if (err)
+            throw new gutil.PluginError("webpack:build", err)
+        gutil.log("[webpack:build]", stats.toString({
+            colors: true
+        }));
+        callback()
     })
 };
 
 
 gulp.task('webpack', (callback) => {
-  let myConfig = Object.create(webpackConfig);
-  return _webpack(myConfig, callback);
+    let myConfig = Object.create(webpackConfig);
+    return _webpack(myConfig, callback);
 });
 
 gulp.task('webpack:prod', (callback) => {
-  let myConfig = Object.create(webpackProd);
-  return _webpack(myConfig, callback);
+    let myConfig = Object.create(webpackProd);
+    return _webpack(myConfig, callback);
 });
 
 gulp.task('build', ['build:misc', 'build:index'])
 
 gulp.task('build:index', () => {
-  let defaultLayout = {
-    layout: "templates/index.jade"
-  };
+    let defaultLayout = {
+        layout: "templates/index.jade"
+    };
 
-  return gulp.src("./templates/index.jade")
-    .pipe(jade())
-    .pipe(gulp.dest('./dist'))
+    return gulp.src("./templates/index.jade")
+        .pipe(jade())
+        .pipe(gulp.dest('./dist'))
 });
 
 gulp.task('serve', () => {
-  gulp.watch(['./templates/**/*.jade', './articles/**/*.md'], () => {
-    runSequence('build', 'compress');
-  });
-  gulp.watch(['./app/**/*',  './sass/**/*.sass'], () => {
-    runSequence('webpack');
-  });
-  return gulp.src('dist')
-    .pipe(webserver({
-      livereload: true,
-      proxies:[{
-        source: '/api/',
-        target: 'http://localhost:9010/api/'
-      }]
-    }));
+    gulp.watch(['./templates/**/*.jade', './articles/**/*.md'], () => {
+        runSequence('build', 'compress');
+    });
+    gulp.watch(['./app/**/*', './sass/**/*.sass'], () => {
+        runSequence('webpack');
+    });
+    return gulp.src('dist')
+        .pipe(webserver({
+            livereload: true,
+            proxies: [{
+                source: '/api/',
+                target: 'http://localhost:9010/api/'
+            }]
+        }));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let path    = require("path");
+let path = require("path");
 let webpack = require("webpack");
 module.exports = {
     context: __dirname + "/app",
@@ -10,14 +10,29 @@ module.exports = {
         filename: "bundle.js",
     },
     module: {
-        loaders: [
-            { test: /\.cjsx$/, loaders: ['coffee', 'cjsx']},
-            { test: /\.coffee$/, loader: "coffee-loader" },
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-            { test: /\.(gif|png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
-            { test: /\.sass$/, loader: "style!css!sass?indentedSyntax" },
-            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "babel-loader", query: { presets: ['react', 'es2015'] }}
-        ]
+        loaders: [{
+            test: /\.cjsx$/,
+            loaders: ['coffee', 'cjsx']
+        }, {
+            test: /\.coffee$/,
+            loader: "coffee-loader"
+        }, {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }, {
+            test: /\.(gif|png|woff|woff2|eot|ttf|svg)$/,
+            loader: 'url-loader?limit=100000'
+        }, {
+            test: /\.sass$/,
+            loader: "style!css!sass?indentedSyntax"
+        }, {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
     },
     resolve: {
         root: [path.join(__dirname, "bower_components")]
